35f81958f3c6a4ede4b87e24736be35952bd617e
Lob <37583137+Lob2018@users.noreply.github.com>
2023-05-03 21:53:15 +0200

    Feat : Front-End : Register +Register

--- front/src/app/auth/components/register/register.component.ts
+++ front/src/app/auth/components/register/register.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TokenResponse } from 'src/app/core/models/auth/token.interface';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { ErrorHandlerService } from 'src/app/core/services/error-handler.service';
+import { LocalStorageService } from 'src/app/core/services/local-storage.service';
+import { Location } from '@angular/common';
+import { RegisterRequest } from 'src/app/core/models/auth/registerRequest.interface';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  public hide = true;
+  public onError = false;
+  public usernamePattern = /^[\w]{1,254}$/;
+  public passwordPattern =
+    /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\!\@\#\&\(\)\-\[\{\}\]\:\;\'\,\?\/\*\~\$\^\+\=\<\>]).{8,254}$/;
+  public form = this.fb.group({
+    username: [
+      '',
+      [Validators.required, Validators.pattern(this.usernamePattern)],
+    ],
+    email: ['', [Validators.required, Validators.email]],
+    password: [
+      '',
+      [Validators.required, Validators.pattern(this.passwordPattern)],
+    ],
+  });
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private errorHandler: ErrorHandlerService,
+    private fb: FormBuilder,
+    private router: Router,
+    private localStorageService: LocalStorageService,
+    private location: Location
+  ) {}
 
-  ngOnInit(): void {
+  public submit(): void {
+    const registerRequest = this.form.value as RegisterRequest;
+    this.authService.register(registerRequest).subscribe({
+      next: (response: TokenResponse) => {
+        this.localStorageService.setToken(response.token);
+        this.router.navigate(['/posts']);
+      },
+      error: (error) => this.errorHandler.handleError(error),
+    });
   }
 
+  /**
+   * Return to previous page
+   */
+  goBack() {
+    this.location.back();
+  }
 }
